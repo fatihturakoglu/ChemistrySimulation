1295ffe24d1db88a9f7a6b4dba70ab41770ba834
Muhammed Fatih Turakoğlu <turakoglufatih@gmail.com>
2025-12-27 14:57:21 +0300

    Kitap Eklendi ilk sayfası yazıldı devamı gelicek

---
+++ Assets/Scripts/BookController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookController : MonoBehaviour
+{
+    public GameObject kitapPaneli; // Canvas içindeki Panel
+
+    void Update()
+    {
+        // Fare sol tık basıldığında
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Objeyi ismiyle değil, "Kitap" etiketiyle kontrol ediyoruz
+                if (hit.collider.CompareTag("ChemistryBook"))
+                {
+                    KitabiAc();
+                }
+            }
+        }
+    }
+
+    public void KitabiAc()
+    {
+        kitapPaneli.SetActive(true);
+        // Fareyi serbest bırakmak (UI etkileşimi için önemli olabilir)
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void KitabiKapat()
+    {
+        kitapPaneli.SetActive(false);
+        // FPS kontrolü varsa burada fareyi tekrar kilitleyebilirsiniz
+    }
+}
